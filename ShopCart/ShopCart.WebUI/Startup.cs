3f03241bc029ea6a78f042fd744709231e28ba23
Eugene Milford <gmilford321@gmail.com>
2021-06-14 20:22:57 +0200

    Initial solution setup

---
+++ ShopCart/ShopCart.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopCart.WebUI.Startup))]
+namespace ShopCart.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
