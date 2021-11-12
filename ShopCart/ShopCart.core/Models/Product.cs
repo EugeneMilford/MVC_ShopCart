d4f8df1c4445392d4a33d452ef22b889ac2ee09b
Eugene Milford <gmilford321@gmail.com>
2021-11-12 19:08:50 +0200

    Product Manager Pages Buiilt and Tested

--- ShopCart/ShopCart.core/Models/Product.cs
+++ ShopCart/ShopCart.core/Models/Product.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using ShopCart.core.Models;
 
 namespace ShopCart.core.Models
 {
